c03b38c7e71cf34e9ba4d04f74721cb7d5350f56
unknown <alfredomarenco@boletea.com>
2021-10-06 12:04:41 -0500

    Agregamos el email como parametro de busqueda en el los leads

--- app/Http/Livewire/Leads/LeadsTable.php
+++ app/Http/Livewire/Leads/LeadsTable.php
@@ -24,17 +24,17 @@ class LeadsTable extends Component
     public function render()
     {
         if (auth()->user()->hasRole('Administrador') && $this->all == true) {
-            $leads = Lead::orderBy($this->sortBy, $this->sortDirection)->where('name', 'like', '%' . $this->search . '%')->where('user_id', '!=', 3)->paginate($this->paginate);
+            $leads = Lead::orderBy($this->sortBy, $this->sortDirection)->where('name', 'like', '%' . $this->search . '%')->orWhere('email','like','%'.$this->search.'%')->where('user_id', '!=', 3)->paginate($this->paginate);
         } elseif (auth()->user()->hasRole('SuperUser') && $this->all == true) {
-            $leads = Lead::orderBy($this->sortBy, $this->sortDirection)->where('name', 'like', '%' . $this->search . '%')->paginate($this->paginate);
+            $leads = Lead::orderBy($this->sortBy, $this->sortDirection)->where('name', 'like', '%' . $this->search . '%')->orWhere('email','like','%'.$this->search.'%')->paginate($this->paginate);
         } elseif ($this->user && $this->status) {
-            $leads = Lead::where('user_id', $this->user)->where('status_id', $this->status)->orderBy($this->sortBy, $this->sortDirection)->where('name', 'like', '%' . $this->search . '%')->paginate($this->paginate);
+            $leads = Lead::where('user_id', $this->user)->where('status_id', $this->status)->orderBy($this->sortBy, $this->sortDirection)->where('name', 'like', '%' . $this->search . '%')->orWhere('email','like','%'.$this->search.'%')->paginate($this->paginate);
         } elseif ($this->user) {
-            $leads = Lead::where('user_id', $this->user)->orderBy($this->sortBy, $this->sortDirection)->where('name', 'like', '%' . $this->search . '%')->paginate($this->paginate);
+            $leads = Lead::where('user_id', $this->user)->orderBy($this->sortBy, $this->sortDirection)->where('name', 'like', '%' . $this->search . '%')->orWhere('email','like','%'.$this->search.'%')->paginate($this->paginate);
         } elseif ($this->status) {
-            $leads = Lead::where('user_id', auth()->user()->id)->where('status_id', $this->status)->orderBy($this->sortBy, $this->sortDirection)->where('name', 'like', '%' . $this->search . '%')->paginate($this->paginate);
+            $leads = Lead::where('user_id', auth()->user()->id)->where('status_id', $this->status)->orderBy($this->sortBy, $this->sortDirection)->where('name', 'like', '%' . $this->search . '%')->orWhere('email','like','%'.$this->search.'%')->paginate($this->paginate);
         } else {
-            $leads = Lead::where('user_id', auth()->user()->id)->orderBy($this->sortBy, $this->sortDirection)->where('name', 'like', '%' . $this->search . '%')->paginate($this->paginate);
+            $leads = Lead::where('user_id', auth()->user()->id)->orderBy($this->sortBy, $this->sortDirection)->where('name', 'like', '%' . $this->search . '%')->orWhere('email','like','%'.$this->search.'%')->paginate($this->paginate);
         }
 
         $users = User::all();
