7e2a49da7b8cf4cab34456f3ab363e50ba2f5000
unknown <alfredomarenco@boletea.com>
2021-10-26 16:11:36 -0500

    Cambios en el logo

--- resources/views/vendor/jetstream/components/application-logo.blade.php
+++ resources/views/vendor/jetstream/components/application-logo.blade.php
@@ -1,1 +1,1 @@
-<img class="w-14 h-14" src="{{ asset('img/logoTerrenosMid.png') }}" >
+<img class="w-14 h-14" src="{{ asset('img/vandu.png') }}" >
--- resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
@@ -1,3 +1,3 @@
 <a href="/">
-    <img class="w-56" src="{{ asset('img/logoTerrenos.svg') }}" alt="">
+    <img class="w-56" src="{{ asset('img/vandu.png') }}" alt="">
 </a>
