118a52ac59346531c186bccc5663c1d70882306d
unknown <alfredomarenco@boletea.com>
2022-04-15 08:18:43 -0500

    Correcciones menores

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -73,26 +73,22 @@
 <body class="antialiased">
 
     <header>
-        <div class="relative bg-white overflow-hidden" x-data="{open:false}" id="inicio">
+        <div class="relative bg-black overflow-hidden" x-data="{open:false}" id="inicio">
             <div class="max-w-7xl mx-auto">
-                <div class="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
-                    <svg class="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2"
-                        fill="currentColor" viewBox="0 0 100 100" preserveAspectRatio="none" aria-hidden="true">
-                        <polygon points="50,0 100,0 50,100 0,100" />
-                    </svg>
+                <div class="relative z-10 pb-8 bg-black sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
                     <div>
-                        <div class="relative pt-6 px-4 sm:px-6 lg:px-8">
+                        <div class="relative pt-6 px-4 sm:px-6 lg:px-2">
                             <nav class="relative flex items-center justify-between sm:h-10 lg:justify-start"
                                 aria-label="Global">
                                 <div class="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
                                     <div class="flex items-center justify-between w-full md:w-auto">
                                         <a href="#">
                                             <span class="sr-only">Workflow</span>
-                                            <img class="w-40" src="{{ asset('img/logoTerrenos.svg') }}">
+                                            <img class="w-52" src="{{ asset('img/logoTerrenosMid.svg') }}">
                                         </a>
                                         <div class="-mr-2 flex items-center md:hidden">
                                             <button x-on:click="open=!open" type="button"
-                                                class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+                                                class="bg-black rounded-md p-2 inline-flex items-center justify-center text-gray-100 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
                                                 aria-expanded="false">
                                                 <span class="sr-only">Open main menu</span>
                                                 <!-- Heroicon name: outline/menu -->
@@ -107,11 +103,11 @@
                                     </div>
                                 </div>
                                 <div class="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
-                                    <a href="#inicio" class="font-medium text-gray-500 hover:text-gray-900">Inicio</a>
+                                    <a href="#inicio" class="font-medium text-white hover:text-gray-100">Inicio</a>
                                     <a href="#desarrollos"
-                                        class="font-medium text-gray-500 hover:text-gray-900">Desarrollos</a>
+                                        class="font-medium text-white hover:text-gray-100">Desarrollos</a>
                                     <a href="#contacto"
-                                        class="font-medium text-gray-500 hover:text-gray-900">Contacto</a>
+                                        class="font-medium text-white hover:text-gray-100">Contacto</a>
                                 </div>
                             </nav>
                         </div>
@@ -138,11 +134,11 @@
                                 </div>
                                 <div class="px-2 pt-2 pb-3 space-y-1">
                                     <a href="#inicio"
-                                        class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Inicio</a>
+                                        class="block px-3 py-2 rounded-md text-base font-medium text-white hover:text-gray-900 hover:bg-gray-50">Inicio</a>
                                     <a href="#desarrollos"
-                                        class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Desarrollos</a>
+                                        class="block px-3 py-2 rounded-md text-base font-medium text-white hover:text-gray-900 hover:bg-gray-50">Desarrollos</a>
                                     <a href="#contacto"
-                                        class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Contacto</a>
+                                        class="block px-3 py-2 rounded-md text-base font-medium text-white hover:text-gray-900 hover:bg-gray-50">Contacto</a>
                                 </div>
                             </div>
                         </div>
@@ -151,19 +147,19 @@
                     <main class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
                         <div class="sm:text-center lg:text-left">
                             <h1
-                                class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl headline">
-                                <span class="block xl:inline">Con Terrenos MID</span>
-                                <span class="block text-green-800 xl:inline">invertir es muy facil</span>
+                                class="text-4xl tracking-tight font-extrabold text-gray-100 sm:text-5xl md:text-6xl headline">
+                                <span class="block xl:inline">Con TerrenosMID</span>
+                                <span class="block text-verde xl:inline">invertir es muy facil</span>
                             </h1>
                             <p
-                                class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+                                class="mt-3 text-base text-gray-100 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
                                 Nuestro grupo de expertos te ayudara a tomar la mejor desicion para la inversion de tu
                                 dinero.
                             </p>
                             <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                                 <div class="rounded-md shadow">
                                     <a href="#contacto"
-                                        class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-green-700 hover:bg-green-600 md:py-4 md:text-lg md:px-10">
+                                        class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-verde hover:bg-white hover:text-black md:py-4 md:text-lg md:px-10">
                                         Hablar con un asesor
                                     </a>
                                 </div>
