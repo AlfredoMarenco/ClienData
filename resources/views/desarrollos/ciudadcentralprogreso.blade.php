dfbb56a20d9b18e95216733d11018252bff1ba73
unknown <alfredomarenco@boletea.com>
2022-04-28 11:17:42 -0500

    Cambios

--- resources/views/desarrollos/ciudadcentralprogreso.blade.php
+++ resources/views/desarrollos/ciudadcentralprogreso.blade.php
@@ -3,7 +3,7 @@
 @section('header1')
     <main class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
         <div class="sm:text-center lg:text-left">
-            <img src="{{ asset('img/logoCiudadCentral.svg') }}" class="w-full object-center object-cover lg:object-contain"
+            <img src="{{ asset('img/logoCiudadCentralProgreso.svg') }}" class="w-full object-center object-cover lg:object-contain"
                 alt="">
         </div>
     </main>
