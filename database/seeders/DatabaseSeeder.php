51fe37fc74e31ac31da99a2fb030a11cd74a44ba
alvarbu <74802594+alvarbu@users.noreply.github.com>
2022-06-23 13:36:15 -0500

    Cambios

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,5 +23,6 @@ class DatabaseSeeder extends Seeder
         $this->call(NoteSeeder::class);
         $this->call(CommentSeeder::class);
         $this->call(EventSeeder::class);
+        $this->call(EmployeeSeeder::class);
     }
 }
