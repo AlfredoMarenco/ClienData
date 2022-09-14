72bc6cc1f3fe9b340a12beb1bdc6918558e0bf47
Alfredo Gonzalez Marenco <alfredomarenco@boletea.com>
2022-09-14 00:23:17 -0500

    Actualizacion de etiqueta

--- resources/views/layouts/terrenos.blade.php
+++ resources/views/layouts/terrenos.blade.php
@@ -2,6 +2,15 @@
 <html lang="es">
 
 <head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-7Z4BSWNZW1"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-7Z4BSWNZW1');
+    </script>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -15,15 +24,6 @@
     {{-- <link rel="stylesheet" href="{{ asset('css/fontello.css') }}"> --}}
     <link rel="icon" type="image/x-icon" href="{{ asset('img/domen/logo-domen.png') }}">
     @livewireStyles
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-7Z4BSWNZW1"></script>
-    <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'G-7Z4BSWNZW1');
-    </script>
 </head>
 
 <body>
