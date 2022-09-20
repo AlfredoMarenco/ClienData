21d292df2a1130e182819ea695aab644369fe6ca
Alfredo Gonzalez Marenco <alfredomarenco@boletea.com>
2022-09-20 17:50:08 -0500

    Update Project Index

--- resources/views/quotes/leads.blade.php
+++ resources/views/quotes/leads.blade.php
@@ -17,7 +17,7 @@
                             </th>
                             <th scope="col"
                                 class="px-6 py-3 text-left text-xs font-bold text-white uppercase tracking-wider cursor-pointer">
-                                Nombre
+                                Accion
                             </th>
                            {{--  <th scope="col" class="relative px-6 py-3">
                                 <span class="sr-only">Action</span>
