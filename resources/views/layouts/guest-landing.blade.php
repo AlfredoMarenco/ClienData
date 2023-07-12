3c1148cbd97004d1915bff9cdcef6ad6925c4a2e
Alvar <alvarbu@gmail.com>
2023-07-12 14:14:28 -0600

    se corrigen estilos

--- resources/views/layouts/guest-landing.blade.php
+++ resources/views/layouts/guest-landing.blade.php
@@ -29,10 +29,10 @@
 
 
     <!-- Styles -->
-    <link rel="stylesheet" href="/css/estilos.css">
+    <link rel="stylesheet" href="css/estilos.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
         integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-    <link rel="stylesheet" href="/css/estilos.css">
+
 
     <!-- Scripts -->
     <script src="{{ mix('js/app.js') }}" defer></script>
