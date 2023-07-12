5acc9f7b650e6007ab477fcee213f43c28f4aa14
Alvar <alvarbu@gmail.com>
2023-07-12 17:24:21 -0600

    se corrigen rutas en contacto

--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -3,7 +3,7 @@
     <header>
         <nav class="navbar navbar-expand-lg navbar-light bg-vandu fixed-top">
             <div class="container">
-                <a class="navbar-brand img-fluid" href="#"><img src="img/2023/vandu-black.svg" alt=""
+                <a class="navbar-brand img-fluid" href="/"><img src="img/2023/vandu-black.svg" alt=""
                         width="100"></a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                     aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -13,10 +13,10 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ml-auto">
                         <li class="nav-item">
-                            <a class="nav-link" href="#header">Inicio <span class="sr-only">(current)</span></a>
+                            <a class="nav-link" href="/">Inicio <span class="sr-only">(current)</span></a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#servicios">Servicios</a>
+                            <a class="nav-link" href="/">Servicios</a>
                         </li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
@@ -28,10 +28,10 @@
                             </div>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#clientes">Clientes</a>
+                            <a class="nav-link" href="/">Clientes</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#aliados">Aliados</a>
+                            <a class="nav-link" href="/">Aliados</a>
                         </li>
                     </ul>
                 </div>
