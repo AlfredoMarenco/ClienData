3ce94dbf3a7cdcdf4824c2cb798c2d75556b38cc
Alvar <alvarbu@gmail.com>
2023-08-02 02:03:48 -0600

    smooth scroll funcionando

--- resources/views/layouts/guest-landing.blade.php
+++ resources/views/layouts/guest-landing.blade.php
@@ -148,7 +148,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js"
         integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous">
     </script>
-    {{--     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> --}}
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script>
         $(document).ready(function() {
             // Add smooth scrolling to all links
