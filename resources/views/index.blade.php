941030e3dbed59c1f75a1f60bf7e844e9c021eae
Alvar Buenfil <alvarbuenfil@MacBook-Air-de-Alvar.local>
2024-01-03 20:16:52 -0600

    cambio de numero te telefono en footer

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,121 +1,112 @@
 @extends('layouts.guest-landing')
 @section('image', 'https://agenciavandu.com/img/1.jpg')
 @section('content')
-  
-    <header id="header">
-        <div class="container">
-            <div class="titular-uno">
-                <div class="text-center">
-                    <h1>Conectamos tecnología y estrategia <br class="d-none d-sm-none d-md-block d-lg-block"> para generar
-                        leads de <span class="enfasis"> alto impacto</span></h1>
-                    <p>Somos una agencia de marketing digital que utiliza la tecnología a tu <br
-                            class="d-none d-sm-none d-md-block d-lg-block"> favor para elevar la
-                        experiencia de atención a clientes y obtener leads <br
-                            class="d-none d-sm-none d-md-block d-lg-block"> de manera sostenida y eficiente</p>
-                    <a href="/contactar-a-un-asesor" class="btn btn-primary">Cotiza con nosotros</a>
-                </div>
-            </div>
-        </div>
-    </header>
-    <section id="servicios">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-7 col-md-12 col-sm-12 m-auto uno">
-                    <h2>Te ayudamos a gestionar <br class="d-none d-sm-none d-md-block d-lg-block"> la relación con tus
-                        clientes</h2>
-                    <p>Un cliente que se siente entendido es un cliente feliz, <b>nuestro equipo de Community Manager</b> se
-                        encarga de encontrarle amigos a tu marca y de mantener esa relación a lo largo del tiempo.</p>
-                </div>
-                <div class="col-lg-5 col-md-12 col-sm-12 dos">
-                    <div class="text-center">
-                        <img src="{{ asset('img/2023/logos-1.svg') }}" width="100%" height="auto" alt="Manychat logo">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <section>
-        <div class="titular-dos">
-            <div class="container">
-                <div class="text-center">
-                    <h2>Creamos secuencias, flujos conversacionales <br class="d-none d-sm-none d-md-block d-lg-block"> e
-                        identificamos palabras clave</h2>
-                    <p>Analizamos la información sobre conversaciones, preguntas frecuentes, solicitudes y dudas a lo largo
-                        del tiempo <br class="d-none d-sm-none d-md-block d-lg-block"> para nutrir la atención al cliente,
-                        automatizar procesos de ventas, generar clientes potenciales y fomentar la <br
-                            class="d-none d-sm-none d-md-block d-lg-block"> fidelidad de los clientes. Además, brindamos la
-                        ventaja de estar disponibles las 24 horas y recopilar datos valiosos <br
-                            class="d-none d-sm-none d-md-block d-lg-block">para mejorar la estrategia de marketing de tu
-                        empresa.</p>
-                    <img src="{{ asset('img/2023/secuencias.webp') }}" class="img-fluid"
-                        alt="Ejemplo de secuencia manychat por agenciavandu.com">
-                </div>
-            </div>
-        </div>
-    </section>
-    <section id="soluciones">
-        <div class="container">
-            <div class="titular-tres">
-                <div class="row">
-                    <div class="col-lg-7 col-md-12 col-sm-12 m-auto">
-                        <h2>Diseñamos y desarrollamos <br class="d-none d-sm-none d-md-block d-lg-block"> páginas web y
-                            sistemas que atrapan</h2>
-                        <p>El contenido, tiempo de retención y de carga son aspectos clave para el posicionamiento de tu
-                            empresa. Nos encargamos de usar las mejores prácticas para que puedas lograr tus objetivos
-                            mensuales.</p>
-                    </div>
-                    <div class="col-lg-5 col-md-12 col-sm-12">
-                        <div class="text-center">
-                            <img src="{{ asset('img/2023/web.webp') }}" width="auto" height="428"
-                                alt="Portal de pagos por agenciavandu.com">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <section id="aliados">
-        <div class="aliados">
-            <div class="container">
-                <div class="titular-cuatro text-center">
-                    <h2>Una agencia validada por</h2>
-                    <div class="d-none d-sm-none d-md-block d-lg-block">
-                        <img src="{{ asset('img/2023/aliados.svg') }}" width="100%" height="auto"
-                            alt="Aliados de agenciavandu.com">
-                    </div>
-                    <div class="d-block d-sm-block d-md-none d-lg-none">
-                        <img src="{{ asset('img/2023/m-aliados.svg') }}" width="100%" height="auto"
-                            alt="aliados agenciavandu.com">
-                    </div>
-                </div>
-                
-            </div>
-        </div>
-    </section>
-    <section id="clientes">
-        <div class="titular-cinco">
-            <div class="container">
-                <div class="text-center">
-                    <h2>Generamos verdaderas <br class="d-none d-sm-none d-md-block d-lg-block"> estrategias digitales</h2>
-                    <p>Imagina que estás participando en una carrera de autos en un circuito de alta velocidad.<br
-                            class="d-none d-sm-none d-md-block d-lg-block">Para tener éxito en esta competencia, necesitas
-                        una estrategia bien <br class="d-none d-sm-none d-md-block d-lg-block"> planificada que se asemeje a
-                        la forma en que un conductor hábil aborda la pista.</p>
-                    <a href="/contactar-a-un-asesor" class="btn btn-primary">Platica con nosotros</a>
-                </div>
-            </div>
-        </div>
-    </section>
-    <section>
-        <div class="titular-seis mb-5">
-            <div class="container">
-                <div class="text-center">
-                    <h2>Ellos confían en nosotros</h2>
-                    <img src="{{ asset('img/2023/clientes.svg') }}" width="100%" height="auto" alt="Logos clientes | tokio 07, Kananfleet, Burger king, Pizza Hut">
 
+<header id="header">
+    <div class="container">
+        <div class="titular-uno">
+            <div class="text-center">
+                <h1>Conectamos tecnología y estrategia <br class="d-none d-sm-none d-md-block d-lg-block"> para generar
+                    leads de <span class="enfasis"> alto impacto</span></h1>
+                <p>Somos una agencia de marketing digital que utiliza la tecnología a tu <br class="d-none d-sm-none d-md-block d-lg-block"> favor para elevar la
+                    experiencia de atención a clientes y obtener leads <br class="d-none d-sm-none d-md-block d-lg-block"> de manera sostenida y eficiente</p>
+                <a href="/contactar-a-un-asesor" class="btn btn-primary">Cotiza con nosotros</a>
+            </div>
+        </div>
+    </div>
+</header>
+<section id="servicios">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-7 col-md-12 col-sm-12 m-auto uno">
+                <h2>Te ayudamos a gestionar <br class="d-none d-sm-none d-md-block d-lg-block"> la relación con tus
+                    clientes</h2>
+                <p>Un cliente que se siente entendido es un cliente feliz, <b>nuestro equipo de Community Manager</b> se
+                    encarga de encontrarle amigos a tu marca y de mantener esa relación a lo largo del tiempo.</p>
+            </div>
+            <div class="col-lg-5 col-md-12 col-sm-12 dos">
+                <div class="text-center">
+                    <img src="{{ asset('img/2023/logos-1.svg') }}" width="100%" height="auto" alt="Manychat logo">
                 </div>
             </div>
         </div>
-    </section>
-    
+    </div>
+</section>
+<section>
+    <div class="titular-dos">
+        <div class="container">
+            <div class="text-center">
+                <h2>Creamos secuencias, flujos conversacionales <br class="d-none d-sm-none d-md-block d-lg-block"> e
+                    identificamos palabras clave</h2>
+                <p>Analizamos la información sobre conversaciones, preguntas frecuentes, solicitudes y dudas a lo largo
+                    del tiempo <br class="d-none d-sm-none d-md-block d-lg-block"> para nutrir la atención al cliente,
+                    automatizar procesos de ventas, generar clientes potenciales y fomentar la <br class="d-none d-sm-none d-md-block d-lg-block"> fidelidad de los clientes. Además, brindamos la
+                    ventaja de estar disponibles las 24 horas y recopilar datos valiosos <br class="d-none d-sm-none d-md-block d-lg-block">para mejorar la estrategia de marketing de tu
+                    empresa.</p>
+                <img src="{{ asset('img/2023/secuencias.webp') }}" class="img-fluid" alt="Ejemplo de secuencia manychat por agenciavandu.com">
+            </div>
+        </div>
+    </div>
+</section>
+<section id="soluciones">
+    <div class="container">
+        <div class="titular-tres">
+            <div class="row">
+                <div class="col-lg-7 col-md-12 col-sm-12 m-auto">
+                    <h2>Diseñamos y desarrollamos <br class="d-none d-sm-none d-md-block d-lg-block"> páginas web y
+                        sistemas que atrapan</h2>
+                    <p>El contenido, tiempo de retención y de carga son aspectos clave para el posicionamiento de tu
+                        empresa. Nos encargamos de usar las mejores prácticas para que puedas lograr tus objetivos
+                        mensuales.</p>
+                </div>
+                <div class="col-lg-5 col-md-12 col-sm-12">
+                    <div class="text-center">
+                        <img src="{{ asset('img/2023/web.webp') }}" width="auto" height="428" alt="Portal de pagos por agenciavandu.com">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section id="aliados">
+    <div class="aliados">
+        <div class="container">
+            <div class="titular-cuatro text-center">
+                <h2>Una agencia validada por</h2>
+                <div class="d-none d-sm-none d-md-block d-lg-block">
+                    <img src="{{ asset('img/2023/aliados.svg') }}" width="100%" height="auto" alt="Aliados de agenciavandu.com">
+                </div>
+                <div class="d-block d-sm-block d-md-none d-lg-none">
+                    <img src="{{ asset('img/2023/m-aliados.svg') }}" width="100%" height="auto" alt="aliados agenciavandu.com">
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+<section id="clientes">
+    <div class="titular-cinco">
+        <div class="container">
+            <div class="text-center">
+                <h2>Generamos verdaderas <br class="d-none d-sm-none d-md-block d-lg-block"> estrategias digitales</h2>
+                <p>Imagina que estás participando en una carrera de autos en un circuito de alta velocidad.<br class="d-none d-sm-none d-md-block d-lg-block">Para tener éxito en esta competencia, necesitas
+                    una estrategia bien <br class="d-none d-sm-none d-md-block d-lg-block"> planificada que se asemeje a
+                    la forma en que un conductor hábil aborda la pista.</p>
+                <a href="/contactar-a-un-asesor" class="btn btn-primary">Platica con nosotros</a>
+            </div>
+        </div>
+    </div>
+</section>
+<section>
+    <div class="titular-seis mb-5">
+        <div class="container">
+            <div class="text-center">
+                <h2>Ellos confían en nosotros</h2>
+                <img src="{{ asset('img/2023/clientes.svg') }}" width="100%" height="auto" alt="Logos clientes | tokio 07, Kananfleet, Burger king, Pizza Hut">
+
+            </div>
+        </div>
+    </div>
+</section>
+
 @endsection
--- resources/views/layouts/guest-landing.blade.php
+++ resources/views/layouts/guest-landing.blade.php
@@ -15,33 +15,26 @@
 
         gtag('config', 'G-C3B9DFX1T8');
     </script>
-    {{-- styles --}}
+
     <link rel="preload" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" as="styles">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
-        integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
     <link rel="preload" href="css/estilos.css" as="styles">
     <link rel="stylesheet" href="css/estilos.css">
     <link rel="preload" href="" as="styles">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="preload"
-        href="https://fonts.googleapis.com/css2?family=Saira:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-        as="font" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Saira:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-        rel="stylesheet">
+    <link rel="preload" href="https://fonts.googleapis.com/css2?family=Saira:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" as="font" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Saira:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     {{-- end styles --}}
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Vandu - Agencia de Marketing digital y desarrollo web en Mérida</title>
+    <title>Vandu - Agencia de Desarrollo web y Diseño en Mérida</title>
     <link rel=»canonical» href=»http://www.agenciavandu.com» />
-    <meta property="og:title" content="Agencia de Marketing digital y desarrollo web en Mérida &mdash; Vandu">
+    <meta property="og:title" content="Agencia de Desarrollo web y Diseño en Mérida &mdash; Vandu">
     <meta property="og:type" content="website" />
-    <meta name="description"
-        content="Nuestro enfoque es simple pero revolucionario: ofrecemos soluciones digitales que cautivan, inspiran y generan resultados extraordinarios. Si buscas una agencia que eleve tu presencia en línea a niveles insuperables, te invitamos a unirte a nosotros en el viaje hacia el éxito digital." />
-    <meta name="og:description"
-        content="Nuestro enfoque es simple pero revolucionario: ofrecemos soluciones digitales que cautivan, inspiran y generan resultados extraordinarios. Si buscas una agencia que eleve tu presencia en línea a niveles insuperables, te invitamos a unirte a nosotros en el viaje hacia el éxito digital." />
+    <meta name="description" content="Nuestro enfoque es simple pero revolucionario: ofrecemos soluciones digitales que cautivan, inspiran y generan resultados extraordinarios. Si buscas una agencia que eleve tu presencia en línea a niveles insuperables, te invitamos a unirte a nosotros en el viaje hacia el éxito digital." />
+    <meta name="og:description" content="Nuestro enfoque es simple pero revolucionario: ofrecemos soluciones digitales que cautivan, inspiran y generan resultados extraordinarios. Si buscas una agencia que eleve tu presencia en línea a niveles insuperables, te invitamos a unirte a nosotros en el viaje hacia el éxito digital." />
     <meta name="og:url" content="https://www.agenciavandu.com" />
     <meta name="og:site_name" content="Vandu" />
     <meta name="og:image" content="https://agenciavandu.com/img/1.jpg" />
@@ -57,68 +50,39 @@
     <script src="https://kit.fontawesome.com/d63ca50714.js" crossorigin="anonymous"></script>
 
 
-    {{-- 
+
     {!! htmlScriptTagJsApi() !!}
 
     <script type="text/javascript">
         function callbackThen(
             response
-        )  {// read HTTP status console.log(response.status); // read Promise object response.json().then(function(data){ console.log(data); }); } function callbackCatch(error){ console.error('Error:', error) }
+        ) // read HTTP status console.log(response.status); // read Promise object response.json().then(function(data){ console.log(data); }); } function callbackCatch(error){ console.error('Error:', error) }
     </script>
-    {!! htmlScriptTagJsApi(['callback_then' => 'callbackThen', 'callback_catch' => 'callbackCatch']) !!} }
+    {!! htmlScriptTagJsApi(['callback_then' => 'callbackThen', 'callback_catch' => 'callbackCatch']) !!}
 </head>
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-vandu fixed-top">
         <div class="container">
-            <a class="navbar-brand img-fluid" href="/" aria-label="Ir al inicio">
-                <img src="img/2023/vandu-black.svg" alt="" width="100"></a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <a class="navbar-brand img-fluid m-auto" href="/" aria-label="ir a página principal"><img src="img/2023/vandu-black.svg" alt="logotipo agencia vandu" width="100"></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#header">Inicio <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#servicios">Servicios</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
-                            aria-expanded="false">
-                            Soluciones
-                        </a>
-                        <div class="dropdown-menu">
-                            <a href="#" class="dropdown-item">Muy pronto</a>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#clientes">Clientes</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#aliados">Aliados</a>
-                    </li>
-                </ul>
-            </div>
         </div>
     </nav>
+   
     <main>
         @yield('content')
     </main>
 
     @include('partials.footer')
 
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
-        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js"
-        integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous">
     </script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script>
